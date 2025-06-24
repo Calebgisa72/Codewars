cd2a73190c98493a0b154a91cf6958de045f1679
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-06-24 21:38:07 +0200

    Find Unique Number

---
+++ 6kyu/findUnique.py
@@ -0,0 +1,8 @@
+def find_uniq(nums: list):
+    identicalNum = nums[0] if nums[0] == nums[1] else (nums[0] if nums[0] == nums[2] else nums[2])
+    for num in nums:
+        if num != identicalNum:
+            return num
+
+uniq = find_uniq([ 0, 10, 0, 0, 0 ])
+print(uniq)
