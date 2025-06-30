189a0be8648cab5859a497525ef68a1062c2daa0
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-06-30 20:38:41 +0200

    max profit

---
+++ 5kyu/maxProfit.py
@@ -0,0 +1,23 @@
+# MaxProfit
+
+# Imagine you're buying and selling stocks throughout the year. Your job is to find the biggest profit you could make by buying low, and selling high only once.
+
+# Here’s what you’re given: A list of stock prices for each day 👉 [7, 1, 5, 3, 6, 4]
+
+# Here’s what you need to find: The difference between the cheapest price you could have bought the stock and the most expensive price you could have sold it later on.
+
+def maxProfit(stocks):
+    maxNum = stocks[0]
+    minNum = stocks[0]
+    for num in stocks:
+        if num < minNum:
+            maxNum = num
+            minNum = num
+        elif num > maxNum:
+            maxNum = num  
+            
+    return maxNum - minNum
+
+print(maxProfit([7, 1, 5, 3, 6, 4]))
+            
+            
