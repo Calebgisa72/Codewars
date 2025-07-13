2b1bf1b08de8c41bf6c2d02f8e5dc142d1ab194c
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-13 22:50:36 +0200

    path finder

--- 4kyu/pathFinder.py
+++ 4kyu/pathFinder.py
@@ -11,7 +11,7 @@
 #         ".....W",
 #         "....W."
 #     ])
-#     test.assert_equals(path_finder(maze), False, repr(maze))
+#     test.assertequals(path_finder(maze), False, repr(maze))
 
 def path_finder(maze):
     mazeGrid = maze.split('\n')
