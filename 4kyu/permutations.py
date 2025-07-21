73a3bf33ccda086cf1a7a2c783b664e548f27b4c
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-21 11:13:56 +0200

    permitation

---
+++ 4kyu/permutations.py
@@ -0,0 +1,12 @@
+# In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+# Create as many "shufflings" as you can!
+
+# Examples:
+# With input 'ab':
+# Your function should return ['ab', 'ba']
+
+# With input 'abc':
+# Your function should return ['abc','acb','bac','bca','cab','cba']
+
+# def permutations(s):
+    
