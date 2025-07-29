2dfffe10e263bd278568d7c3b37b9e0deb1259bc
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-29 10:42:31 +0200

    spin words

--- 6kyu/spin_words.py
+++ 6kyu/spin_words.py
@@ -2,7 +2,7 @@
 
 # Examples:
 
-# "Hey fellow warriors"  --> "Hey wllef sroirraw" 
+# "Hey fellow warriors"  --> "Hey wollef sroirraw" 
 # "This is a test        --> "This is a test" 
 # "This is another test" --> "This is rehtona test"
 
