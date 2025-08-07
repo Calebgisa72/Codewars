293cb412b89bcc37353fdbeeeeb709449e34fb37
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-08-07 10:23:43 +0200

    spin words

--- 6kyu/spin_words.py
+++ 6kyu/spin_words.py
@@ -4,7 +4,7 @@
 
 # "Hey fellow warriors"  --> "Hey wollef sroirraw" 
 # "This is a test        --> "This is a test" 
-# "This is another test" --> "This is rehto test"
+# "This is another test" --> "This is rehton test"
 
 def spin_words(sentence: str):
     reversedStr = []
