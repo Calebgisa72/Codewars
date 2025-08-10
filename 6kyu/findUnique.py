80987983170a3cb3eedbe300878ef9e01c578a07
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-08-10 20:41:12 +0200

    find unique

--- 6kyu/findUnique.py
+++ 6kyu/findUnique.py
@@ -1,6 +1,6 @@
 # This function finds the unique number in an array of similar numbers
 
-def find_uniq(nums):
+def find_uniq(nums: list):
     identicalNum = nums[0] if nums[0] == nums[1] else (nums[0] if nums[0] == nums[2] else nums[2])
     for num in nums:
         if num != identicalNum:
